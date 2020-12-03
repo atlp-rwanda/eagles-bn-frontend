cc0262e7b5a01860dea619f264aca228cd0c60d1
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-12-03 10:08:48 +0200

    setup redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './redux/reducers'
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <ErrorBoundary>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ErrorBoundary>,
   document.getElementById('root')
 );
