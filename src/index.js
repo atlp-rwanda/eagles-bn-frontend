ee9a09b4a223649ce8cb5658af5a08728e483292
siyubu <solangeiyubu@gmail.com>
2020-12-03 10:34:55 +0200

    Eagles bn front end routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import App from './App';
+import Login from './components/pages/login'
 
 ReactDOM.render(
   <ErrorBoundary>
