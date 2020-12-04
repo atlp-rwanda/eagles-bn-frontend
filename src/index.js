ea3292d100b992df49b7d85ac5aa8d4bb176ff8b
Siyubu <48902138+Siyubu@users.noreply.github.com>
2020-12-04 11:40:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import App from './App';
-import Login from './components/pages/login'
-
 ReactDOM.render(
   <ErrorBoundary>
     <App />
